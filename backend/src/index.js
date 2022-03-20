553d19932dcc5ad1a61895ef6e91a5685d2b177f
Wassim Benzarti <wass11121996@gmail.com>
2022-03-20 17:53:01 +0100

    changes to Dockerfile

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
-
+// comment
 const listener = app.listen(process.env.PORT || 8080, () => {
   console.log("Server started on port: ", listener.address().port);
 });
